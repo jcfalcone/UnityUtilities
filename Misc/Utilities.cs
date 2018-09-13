384a9785826e4c96dc4ef931d1bde08afed68f3e
jcfalcone <jc.falcone@gmail.com>
2018-09-12 22:25:54 -0400

    Changes

---
+++ Misc/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+	static Camera mainCamera;
+
+	public static bool IsInFrontOfCamera(Vector3 _position)
+	{
+		if(mainCamera == null)
+		{
+			mainCamera = Camera.main;
+		}
+
+		if(mainCamera != null)
+		{
+			Vector3 position = mainCamera.WorldToScreenPoint(_position);
+
+			if(position.x > 0 && position.x < 1 &&
+			   position.y > 0 && position.y < 1 && 
+			   position.z > 0)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
---
+++ Scriptable/Event/UpdateNumberToText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Falcone.Events
+{
+	public class UpdateNumberToText : MonoBehaviour 
+	{
+		[SerializeField]
+		Falcone.Variables.IntScriptable text;
+
+		[SerializeField]
+		TextMeshProUGUI targetlbl;	
+		// Update is called once per frame
+		public void UpdateString () 
+		{
+			this.targetlbl.text = this.text.value.ToString();
+		}
+	}
+}
