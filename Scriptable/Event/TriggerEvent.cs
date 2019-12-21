fc99f5bc99172d7469a61f929bcdeec98db71d97
Julio Cesar <jc.falcone@gmail.com>
2019-12-20 19:39:28 -0500

    - Event Trigger

---
+++ Scriptable/Event/TriggerEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent OnEnter;
+
+    [SerializeField]
+    UnityEvent OnExit;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        this.OnEnter.Invoke();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        this.OnExit.Invoke();
+    }
+}
