b632edf07f5293a3f65af9ca1e69efa0d3bad9fe
Julio Cesar <jcfalcone@users.noreply.github.com>
2020-09-19 19:16:58 -0400

    - Added ScrollUtils to the utilities

---
+++ Misc/ScrollUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Falcone
+{
+    public static class ScrollRectExtensions
+    {
+        public static Vector2 GetSnapToPositionToBringChildIntoView(this ScrollRect instance, RectTransform child)
+        {
+            Canvas.ForceUpdateCanvases();
+            Vector2 viewportLocalPosition = instance.viewport.localPosition;
+            Vector2 childLocalPosition = child.localPosition;
+            Vector2 result = new Vector2(
+                0 - (viewportLocalPosition.x + childLocalPosition.x),
+                0 - (viewportLocalPosition.y + childLocalPosition.y)
+            );
+            return result;
+        }
+
+
+        public static void SnapToPositionToBringChildIntoView(this ScrollRect instance, RectTransform child)
+        {
+            Canvas.ForceUpdateCanvases();
+            Vector2 result = new Vector2(
+                0 - (instance.viewport.localPosition.x + child.localPosition.x),
+                0 - (instance.viewport.localPosition.y + child.localPosition.y)
+            );
+
+            instance.content.localPosition = result;
+        }
+    }
+}
